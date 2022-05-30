ae51f328f9970318f1a457823522af5ac529dda2
Aaron Curtis <foobarbecue@gmail.com>
2022-05-29 21:28:52 -0700

    add elevation axis control

--- controller.py
+++ controller.py
@@ -9,23 +9,41 @@ elevation = [LED("BOARD35"), LED("BOARD37")]
 
 imu = IMU(baudrate=115200)
 
-def go_to_setpoint(setpoint, deadband):
+def stop_all():
+    [relay.off() for relay in elevation + azimuth]
+
+def go_to_setpoint(az, el, deadband):
     try:
         while True:
             sleep(0.2)
+
+            z_angle = imu.get_angle()[2]
+            z_err = z_angle - az
+            if abs(z_err) > deadband:
+                if z_err > 0:
+                    azimuth[0].on()
+                    print(f'z error {z_err}, moving az0')
+                else:
+                    azimuth[1].on()
+                    print(f'z error {z_err}, moving az1')
+            else:
+                [relay.off() for relay in azimuth]
+                print(f'z error {z_err}, within deadband')
+
             y_angle = imu.get_angle()[1]
-            y_err = y_angle - setpoint
+            y_err = y_angle - el
             if abs(y_err) > deadband:
                 if y_err > 0:
                     elevation[0].on()
-                    print(f'error {y_err}, moving el0')
+                    print(f'y error {y_err}, moving el0')
                 else:
                     elevation[1].on()
-                    print(f'error {y_err}, moving el1')
+                    print(f'y error {y_err}, moving el1')
             else:
                 [relay.off() for relay in elevation]
-                print(f'error {y_err}, within deadband')
+                print(f'y error {y_err}, within deadband')
+
     finally:
         [relay.off() for relay in elevation]
 
-go_to_setpoint(10, 2)
+go_to_setpoint(10, 10, 2)
