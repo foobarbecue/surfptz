6320781fe4adf41d4eb8d106391bc20009e8cfa1
Aaron Curtis <foobarbecue@gmail.com>
2022-07-04 22:06:45 -0700

    working combined motion test

--- base/surfptz_base/surfptz_test.py
+++ base/surfptz_base/surfptz_test.py
@@ -1,5 +1,5 @@
 from gimbal import BescorGimbal
-# import camera_manager
+from panasonic_camera import camera_manager
 import logging
 from time import sleep
 
@@ -7,8 +7,15 @@ from time import sleep
 logging.basicConfig(level=logging.INFO)
 
 
-# camera_mgr = camera_manager.PanasonicCameraManager(identify_as='surfptz')
+camera_mgr = camera_manager.PanasonicCameraManager(identify_as='surfptz')
+
+camera_mgr.start()
+# camera_mgr.run()
 
 bescor_gimbal = BescorGimbal()
 
-bescor_gimbal.goto(pitch_angle=0, yaw_angle=0)
+while True:
+    camera_mgr.camera.zoom_in_slow()
+    bescor_gimbal.goto(pitch_angle=5, yaw_angle=5)
+    camera_mgr.camera.zoom_out_slow()
+    bescor_gimbal.goto(pitch_angle=0, yaw_angle=0)
