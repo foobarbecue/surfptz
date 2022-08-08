2e07072770b7a64769fafad611172e97c02179f5
Aaron Curtis <aaron.curtis@jpl.nasa.gov>
2022-08-07 21:51:52 -0700

    rejigger dhcpcd hook so root sets up the IMU

--- base/start_surfptz.sh
+++ base/start_surfptz.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 echo starting surfptz
 #source /home/aaron/robot-cameraman-venv/bin/activate
-rfcomm release 00:0C:BF:16:58:31
-rfcomm bind rfcomm0 00:0C:BF:16:58:31
-export PYTHONPATH=/home/aaron/robot-cameraman
-python -m robot_cameraman --gimbal Bescor --detectionEngine Dummy --identifyToPanasonicCameraAs surfptz
+#rfcomm release 00:0C:BF:16:58:31
+#rfcomm bind rfcomm0 00:0C:BF:16:58:31
+export FLASK_APP=/home/aaron/surfptz/base/surfptz_base/server.py
+#python -m robot_cameraman --gimbal Bescor --detectionEngine Dummy --identifyToPanasonicCameraAs surfptz
+python -m flask run --host=0.0.0.0
