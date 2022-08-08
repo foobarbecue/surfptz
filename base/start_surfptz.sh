6f736be358349c4e6dfc6d3069714c023d270909
Aaron Curtis <aaron.curtis@jpl.nasa.gov>
2022-08-07 22:39:59 -0700

    setting pythonpath until we have a deploy process

--- base/start_surfptz.sh
+++ base/start_surfptz.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 echo starting surfptz
 #source /home/aaron/robot-cameraman-venv/bin/activate
-#rfcomm release 00:0C:BF:16:58:31
-#rfcomm bind rfcomm0 00:0C:BF:16:58:31
+rfcomm release 00:0C:BF:16:58:31
+rfcomm bind rfcomm0 00:0C:BF:16:58:31
+export PYTHONPATH=/home/aaron/surfptz/base/surfptz_base/
 export FLASK_APP=/home/aaron/surfptz/base/surfptz_base/server.py
 #python -m robot_cameraman --gimbal Bescor --detectionEngine Dummy --identifyToPanasonicCameraAs surfptz
 python -m flask run --host=0.0.0.0
